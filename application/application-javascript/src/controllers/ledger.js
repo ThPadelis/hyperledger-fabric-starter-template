eb0b6070b24d6d2c136e255ef1acec4fe945d04d
Thpadelis <padelis.theodosiou@gmail.com>
2021-01-12 16:40:37 +0200

    Major updates
    - Updated LedgerController
    - Added new methods
    - Minor changes

--- application/application-javascript/src/controllers/ledger.js
+++ application/application-javascript/src/controllers/ledger.js
@@ -1,6 +1,7 @@
 const { Wallets, Gateway } = require("fabric-network");
 const { buildCCPOrg1, buildWallet } = require("../../../../test-application/javascript/AppUtil");
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
 const walletPath = path.join(__dirname, "..", "wallet");
 const channelName = "mychannel";
@@ -46,8 +47,52 @@ class LedgerController {
         }
     }
 
+    async _createPolicy(request, response, next) {
+        const { userID: org1UserId = "", creationDate = "", hasDataSubject = "", hasPersonalDataCategory = "", hasProcessing = "", hasPurpose = "", hasRecipient = "", hasStorage = { location: "", duration: "" } } = request.body;
+        const id = uuidv4();
+
+        console.log(`About to create new policy and insert it into the ledger`);
+
+        try {
+            const ccp = buildCCPOrg1();
+            const wallet = await buildWallet(Wallets, walletPath);
+
+            const gateway = new Gateway();
+            try {
+                await gateway.connect(ccp, {
+                    wallet,
+                    identity: org1UserId,
+                    discovery: { enabled: true, asLocalhost: true },
+                });
+
+                const network = await gateway.getNetwork(channelName);
+                const contract = network.getContract(chaincodeName);
+
+                console.log("\n--> Submit Transaction: CreatePolicy, function creates and inserts a new policy into the ledger");
+                const newPolicy = await contract.submitTransaction("CreatePolicy", id, creationDate, hasDataSubject, hasPersonalDataCategory, hasProcessing, hasPurpose, hasRecipient, hasStorage.location, hasStorage.duration);
+                console.log("*** Result: committed");
+
+                // console.log("\n--> Evaluate Transaction: ReadPolicy, function fetches a policy from the ledger");
+                // const policy = await contract.submitTransaction("ReadPolicy", id);
+                // console.log("*** Result: committed");
+
+                return response.status(200).json({ policy: id });
+            } catch (error) {
+                return response.status(400).json({
+                    message: `Failed to create policy`,
+                    error
+                });
+            } finally {
+                gateway.disconnect();
+            }
+        } catch (error) {
+            console.error(`******** FAILED to create policy: ${error}`);
+            return response.status(500).json({ message: error.message });
+        }
+    }
+
     async _getPolicies(request, response, next) {
-        const { userID: org1UserId } = request.body;
+        const { userID: org1UserId } = request.query;
 
         console.log(`About to get all policies from the ledger`);
 
@@ -87,8 +132,7 @@ class LedgerController {
     }
 
     async _getPolicy(request, response, next) {
-        const { userID: org1UserId } = request.body;
-        const { id: policyID } = request.params;
+        const { userID: org1UserId, id: policyID } = request.query;
 
         console.log(`About to get policy from the ledger`);
 
@@ -126,6 +170,94 @@ class LedgerController {
         }
 
     }
+
+    async _updatePolicy(request, response, next) {
+        const { userID: org1UserId = "", creationDate = "", hasDataSubject = "", hasPersonalDataCategory = "", hasProcessing = "", hasPurpose = "", hasRecipient = "", hasStorage = { location: "", duration: "" } } = request.body;
+        const { id } = request.params;
+
+        console.log(`About to update an existing policy`);
+
+        try {
+            const ccp = buildCCPOrg1();
+            const wallet = await buildWallet(Wallets, walletPath);
+
+            const gateway = new Gateway();
+            try {
+                await gateway.connect(ccp, {
+                    wallet,
+                    identity: org1UserId,
+                    discovery: { enabled: true, asLocalhost: true },
+                });
+
+                const network = await gateway.getNetwork(channelName);
+                const contract = network.getContract(chaincodeName);
+
+                console.log("\n--> Submit Transaction: UpdatePolicy, function updates an existing policy from the ledger");
+                const updatedPolicy = await contract.submitTransaction("UpdatePolicy", id, creationDate, hasDataSubject, hasPersonalDataCategory, hasProcessing, hasPurpose, hasRecipient, hasStorage.location, hasStorage.duration);
+                console.log("*** Result: committed");
+
+                console.log("\n--> Evaluate Transaction: ReadPolicy, function fetches a policy from the ledger");
+                const policy = await contract.submitTransaction("ReadPolicy", id);
+                console.log("*** Result: committed");
+
+                return response.status(200).json({ policy: JSON.parse(policy) });
+            } catch (error) {
+                return response.status(400).json({
+                    message: `Failed to update policy`,
+                    error
+                });
+            } finally {
+                gateway.disconnect();
+            }
+        } catch (error) {
+            console.error(`******** FAILED to update policy: ${error}`);
+            return response.status(500).json({ message: error.message });
+        }
+    }
+
+    async _deletePolicy(request, response, next) {
+        const { userID: org1UserId } = request.body;
+        const { id: policyID } = request.params;
+
+        console.log(`About to delete policy from the ledger`);
+
+        try {
+            const ccp = buildCCPOrg1();
+            const wallet = await buildWallet(Wallets, walletPath);
+
+            const gateway = new Gateway();
+            try {
+                await gateway.connect(ccp, {
+                    wallet,
+                    identity: org1UserId,
+                    discovery: { enabled: true, asLocalhost: true },
+                });
+
+                const network = await gateway.getNetwork(channelName);
+                const contract = network.getContract(chaincodeName);
+
+                console.log("\n--> Evaluate Transaction: DeletePolicy, function deletes a policy from the ledger");
+                const policy = await contract.evaluateTransaction("DeletePolicy", policyID);
+                console.log("*** Result: committed");
+
+                return response.status(200).json({ message: "Policy deleted" });
+            } catch (error) {
+                return response.status(400).json({
+                    message: `Failed to delete policy from the ledger`,
+                    error
+                });
+            } finally {
+                gateway.disconnect();
+            }
+        } catch (error) {
+            console.error(`******** FAILED to delete policy from the ledger: ${error}`);
+            return response.status(500).json({ message: error.message });
+        }
+
+    }
+
 }
 
 module.exports = LedgerController;
+
+
--- application/application-javascript/src/routes/ledger.js
+++ application/application-javascript/src/routes/ledger.js
@@ -14,18 +14,15 @@ class LedgerRoutes {
 
         app.post('/init', controller._initialize);
 
-        app.post("/policies", controller._getPolicies);
+        app.post("/policies", controller._createPolicy);
 
+        app.get("/policies", controller._getPolicies);
 
-        app.post("/policies/:id", controller._getPolicy);
+        app.get("/policies/:id", controller._getPolicy);
 
-        app.put("/policies/:id", async (request, response, next) => {
+        app.put("/policies/:id", controller._updatePolicy);
 
-        });
-
-        app.delete("/policies/:id", async (request, response, next) => {
-
-        });
+        app.delete("/policies/:id", controller._deletePolicy);
 
         app.use('**', async (request, response, next) => {
             return response.status(404).json({ message: "Not found!" })
